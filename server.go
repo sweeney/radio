a9a2ebdba3a68b4ed7b0c127f054b6241a16cd47
sweeney <martin@sweeney.io>
2024-11-29 22:40:09 +0000

    Remove icy headers to see if that helps yoto

--- server.go
+++ server.go
@@ -244,8 +244,8 @@ func main() {
 
 		w.Header().Set("Content-Type", "audio/mpeg")
 		w.Header().Set("Cache-Control", "no-cache, no-store")
-		w.Header().Set("icy-br", "128")
-		w.Header().Set("ice-audio-info", "channels=2;samplerate=44100;bitrate=128")
+//		w.Header().Set("icy-br", "128")
+//		w.Header().Set("ice-audio-info", "channels=2;samplerate=44100;bitrate=128")
 		w.Header().Set("icy-name", "Podcast Stream")
 
 		clientCh := server.AddClient(getClientIP(r))
